9e5561605155c1a6fa86a7f4f2ecaa11a9828809
jared.gaertner <jared.gaertner@gmail.com>
2020-04-06 01:27:34 -0700

    Adding in profiles for backend and executable jar

--- backend/src/main/java/com/cloudpractice/interview/CorsFilter.java
+++ backend/src/main/java/com/cloudpractice/interview/CorsFilter.java
@@ -1,5 +1,7 @@
 package com.cloudpractice.interview;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
@@ -8,12 +10,18 @@ import java.io.IOException;
 
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
+    @Value("${spring.profiles.active:prod}")
+    private String activeProfile;
 
     @Override
     public void filter(ContainerRequestContext requestContext,
                        ContainerResponseContext responseContext) throws IOException {
+        String url = "http://34.217.27.245:8080";
+        if (activeProfile == "local") {
+            url = "http://localhost:8090";
+        }
         responseContext.getHeaders().add(
-                "Access-Control-Allow-Origin", "http://localhost:8090");
+                "Access-Control-Allow-Origin", "*");
         responseContext.getHeaders().add(
                 "Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add(
